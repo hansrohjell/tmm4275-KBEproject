f55cdb489e5fe49966cd827d51fa381c0d60f0ef
SvenThorkildsen <78145109+SvenThorkildsen@users.noreply.github.com>
2021-03-17 19:58:17 +0100

    Update readDFA.py
    Fikset linje-parametere

--- Project 2-potential_weld_lines/readDFA.py
+++ Project 2-potential_weld_lines/readDFA.py
@@ -1,7 +1,8 @@
 def line_generator(length, width, originX, originY, iterator):
     counter = 4 * iterator
-    #Regne ut koordinater her? Blir appendet i neste linje
-    weldLines = "(Child) line_"+str(counter+1)+": {\n Class, ug_line; \n Start_Point, Point("+str(int(originX))+", "+str(int(originY))+", fZ:); \n End_Point, Point("+str(int(originX+length))+", "+str(int(originY))+", fZ:); \n};\n\n(Child) line_"+str(counter+2)+": {\n Class, ug_line; \n Start_Point, Point("+str(int(originX+length))+", "+str(int(originY))+", fZ:); \n End_Point, Point("+str(int(originX+length))+", "+str(int(originY+width))+", fZ:); \n};\n\n(Child) line_"+str(counter+3)+": {\n Class, ug_line; \n Start_Point, Point("+str(int(originX+length))+", "+str(int(originY+width))+", fZ:); \n End_Point, Point("+str(int(originX))+", "+str(int(originY+width))+", fZ:); \n};\n\n(Child) line_"+str(counter+4)+": {\n Class, ug_line; \n Start_Point, Point("+str(int(originX))+", "+str(int(originY+width))+", fZ:); \n End_Point, Point("+str(int(originX))+", "+str(int(originY))+", fZ:); \n};\n\n"
+    dx = originX + length
+    dy = originY + width
+    weldLines = "(Child) line_"+str(counter+1)+": {\n Class, ug_line; \n Start_Point, Point("+str(originX)+", "+str(originY)+", fZ:); \n End_Point, Point("+str(dx)+", "+str(originY)+", fZ:); \n};\n\n(Child) line_"+str(counter+2)+": {\n Class, ug_line; \n Start_Point, Point("+str(dx)+", "+str(originY)+", fZ:); \n End_Point, Point("+str(dx)+", "+str(dy)+", fZ:); \n};\n\n(Child) line_"+str(counter+3)+": {\n Class, ug_line; \n Start_Point, Point("+str(dx)+", "+str(dy)+", fZ:); \n End_Point, Point("+str(originX)+", "+str(dy)+", fZ:); \n};\n\n(Child) line_"+str(counter+4)+": {\n Class, ug_line; \n Start_Point, Point("+str(originX)+", "+str(dy)+", fZ:); \n End_Point, Point("+str(originX)+", "+str(originY)+", fZ:); \n};\n\n"
     fileString = ""
     fileString += weldLines    
     return fileString
@@ -12,7 +13,7 @@ def read_DFA(dfa_template):
     file = open(dfa_template)
     s = ""
     for line in file:
-        s+= line # adding each line in a string.
+        s += line # adding each line in a string.
         
     file.close()
 
@@ -35,7 +36,7 @@ def read_DFA(dfa_template):
 
         print("\nWall " + str(i+1) + "\n Length: " + str(length) + "\n Width: " + str(width) + "\n OriginX: " + str(originX) + "\n OriginY: " + str(originY) + "\n")      
         
-        weldLines = line_generator(length, width, originX, originY, i)
+        weldLines = line_generator(int(length), int(width), int(originX), int(originY), i)
         s += weldLines
         
         i += 1
